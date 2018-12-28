edc2afcce270ab525decd5a10031669161e52d10
liujianchuan <liujianchuan@jd.com>
2018-12-28 17:27:02 +0800

    feature: legacy

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-
+import './Demo.css';
 export interface InterProps {
     name: string;
     price: number;
     onIncrement?: () => void;
     onDecrement?: () => void;
 }
+
 function Demo({
     name,
     price,
@@ -13,10 +14,10 @@ function Demo({
     onDecrement
 }: InterProps){
 
-    return (<div>
+return (<div>
         <span>{`name: ` + name + ` price: ` + price}</span><br/>
         <input onClick={onIncrement} value="加价" type="button"/><br/>
-        <input onClick={onDecrement} value="加价" type="button"/>
+        <input onClick={onDecrement} value="减价" type="button"/>
     </div>)
 }
 export default Demo
--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,9 @@ import {store} from './store';
 import Demo from './store/demo/index';
 import DemoUsage from './components/Demo.usage';
 ReactDOM.render(
-    <Provider store={store}>
-      <Demo />
-      <DemoUsage  name={`hello`} price={1000}/>
-    </Provider>,
-    document.getElementById('root') as HTMLElement
-  );
+  <Provider store={store}>
+    <Demo />
+    <DemoUsage  name={`hello`} price={1000}/>
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
